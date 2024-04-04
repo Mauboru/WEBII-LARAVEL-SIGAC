ac4820e13cb3cdc556f9c6ea644c8e0151035100
Josue Henrique <josue21henrique@gmail.com>
2024-04-04 00:52:14 -0300

    delete category ok

--- app/Http/Controllers/CategoriaController.php
+++ app/Http/Controllers/CategoriaController.php
@@ -85,11 +85,15 @@ class CategoriaController extends Controller {
     }
 
     public function destroy(string $id) {
-        
         if($this->repository->delete($id))  {
-            return "<h1>Delete - OK!</h1>";
+            return redirect()->route('categoria.index');
         }
         
-        return "<h1>Delete - Not found Turma!</h1>";
+        return view('message')
+                ->with('template', "main")
+                ->with('type', "danger")
+                ->with('titulo', "OPERAÇÃO INVÁLIDA")
+                ->with('message', "Não foi possível efetuar o procedimento!")
+                ->with('link', "categoria.index");
     }
 }
