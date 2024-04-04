c710061d1aa154a4c3155c849fcfef548be2998e
Josue Henrique <josue21henrique@gmail.com>
2024-04-04 01:20:01 -0300

    view create/store nivel ok

---
+++ resources/views/nivel/create.blade.php
@@ -0,0 +1,16 @@
+@extends('templates/main', ['titulo'=>"NOVO NIVEL"])
+
+@section('conteudo')
+    <form action="{{ route('nivel.store') }}" method="POST">
+        @csrf
+        <x-textbox name="nome" label="Nome" type="text" value="null" disabled="false"/>
+        <div class="row">
+            <div class="col text-start">
+                <x-button label="Voltar" type="link" route="curso.index" color="secondary"/>
+            </div>
+            <div class="col text-end">
+                <x-button label="Cadastar" type="submit" route="" color="success"/>
+            </div>
+        </div>
+    </form>
+@endsection
