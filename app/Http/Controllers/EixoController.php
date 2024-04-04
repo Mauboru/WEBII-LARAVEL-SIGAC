e84e724435307e8d42ef5fbc9bf8861338cc9511
Josue Henrique <josue21henrique@gmail.com>
2024-04-04 01:39:00 -0300

    view destroy/delete eixo ok

--- app/Http/Controllers/EixoController.php
+++ app/Http/Controllers/EixoController.php
@@ -76,9 +76,14 @@ class EixoController extends Controller {
 
     public function destroy(string $id) {
         if($this->repository->delete($id))  {
-            return "<h1>Delete - OK!</h1>";
+            return redirect()->route('eixo.index'); 
         }
         
-        return "<h1>Delete - Not found Eixo!</h1>";
+        return view('message')
+            ->with('template', "main")
+            ->with('type', "danger")
+            ->with('titulo', "OPERAÇÃO INVÁLIDA")
+            ->with('message', "Não foi possível efetuar o procedimento!")
+            ->with('link', "curso.index");
     }
 }
