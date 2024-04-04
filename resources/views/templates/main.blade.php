7bb0a54927d7981d6255b624911ca3657bc6703d
Josue Henrique <josue21henrique@gmail.com>
2024-04-04 12:20:29 -0300

    CRUD VIEWS professor ok

--- resources/views/templates/main.blade.php
+++ resources/views/templates/main.blade.php
@@ -87,7 +87,7 @@
 								</a> 
 							</li>
 							<li class="sidebar-item"> 
-								<a href="#" class="sidebar-link"> 
+								<a href="{{ route('users.role', 'PROFESSOR') }}" class="sidebar-link"> 
 									<span class="ms-3">Professores</span> 
 								</a> 
 							</li>
